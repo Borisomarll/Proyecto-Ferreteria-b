d5f8c9aaf4c2c8fefedef0f5dc5db0b7aee81d2c
Boris Omar <borisomarllv82@gmail.com>
2024-12-07 19:31:26 -0500

    Agregar archivos de proyecto.

---
+++ CapaEntidades/entProducto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    internal class entProducto
+    {
+        public int IdProducto { get; set; }
+        public required string Nombre { get; set; }
+        public required string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int CantidadEnStock { get; set; }
+        public int IdCategoria { get; set; }
+    }
+}
